919d59d44706bac27b9fa9d72e78307379f3c49b
chgr007 <chgr007@student.kristiania.no>
2022-11-15 09:40:30 +0100

    forhåpentligvis funker workflow

--- terraform-demo/s3.tf
+++ terraform-demo/s3.tf
@@ -5,21 +5,28 @@ terraform {
       version = "4.33.0"
     }
   }
+  backend "s3" {
+    bucket = "pgr301-2021-terraform-state"
+    key    = "csg/apprunner-lab.state"
+    region = "eu-north-1"
+  }
 }
 
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucket_name
 }
 
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.mybucket.bucket
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+      sse_algorithm = "AES256"
     }
   }
 }
 
 variable "bucket_name" {
-  type = string
+  type    = string
+  default = "csg-s3bucket"
 }
